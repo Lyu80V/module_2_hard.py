5293c88c600cfa19dfc965a3f154f8f5f253a95e
Lyu80V <7222707@gmail.com>
2024-08-24 13:32:55 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,7 +1,6 @@
 import random
 n = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 win = random.choice(n)
-#n = int(input('Введите число от 3 до 20:'))
 result = []
 for i in range(1, win):
     for j in range(1,win):
